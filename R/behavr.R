a8779e761f59fcbe2a4ca30e7e703a13b5bb8532
Quentin Geissmann <qgeissmann@gmail.com>
2017-07-31 22:39:07 +0100

    Update behavr.R

--- R/behavr.R
+++ R/behavr.R
@@ -24,13 +24,13 @@ NULL
 #' @param data todo
 #' @param metadata todo
 #' @export
-
+#library(data.table)
+#setClass("behavr", contains = "data.table")
 behavr <- function(x, metadata){
-  #todo, d[,t] should be hms
-  #out <- new("behavr", x)
+  #todo, check input type/key/column names...!!
   # todo, run checks on meta data and data
   data.table::setattr(x,"metadata",metadata)
-  new("behavr", x)
+  data.table::setattr(x,"class",c("behavr","data.table","data.frame")) 
 }
 
 meta <- function(d, with_n=T, with_t_range=T){
@@ -50,20 +50,6 @@ xmd <- function(var){
 #-------------- Indexing
 
 "[.behavr" <- function(x,...){
-      mc <- match.call()
-      print(1111)
-      #mc$x <- substitute(S3Part(x, strictS3 = TRUE))
-      dt <- S3Part(x, strictS3 = TRUE)
-      # find out if we have inline asignment here... :|
-      behavr(dt[...], metadata=meta(x))
-
-      #here, warning if no time?!
-      }
-
-# set.seed(1)
-# met <- data.table::data.table(id = 1:5, condition=letters[1:5], sex=c("M","M","M","F", "F"), key="id")
-# t <- 1L:100L
-# data <- met[,list(t=t, x=rnorm(100),y=rnorm(100), eating=runif(100) > .5 ),by="id"]
-#
-# d <- behavr(data,met)
-# d[1,1]
+ out <- NextMethod()
+ behavr(out,metadata=meta(x))
+}
