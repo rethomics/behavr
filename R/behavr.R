fe1f10bff0dd516db8d29c98757ee99b251b9bab
Quentin Geissmann <qgeissmann@gmail.com>
2017-08-01 11:48:10 +0100

    xmd doc

--- R/behavr.R
+++ R/behavr.R
@@ -3,7 +3,8 @@
 
 #library(data.table)
 #' @importFrom data.table data.table
-setClass("behavr", contains = "data.table")
+#' @importFrom methods setOldClass
+setOldClass(c("behavr", "data.table"))
 
 #' todo
 #'
@@ -11,45 +12,50 @@ setClass("behavr", contains = "data.table")
 #'
 #' @name behavr
 #' @examples
-#' \dontrun{
-#'   # Will raise an error
-#'   #todo
-#' }
+#' set.seed(1)
+#' met <- data.table::data.table(id = 1:5,
+#'                               condition=letters[1:5],
+#'                               sex=c("M","M","M","F", "F"),
+#'                               key="id")
+#' t <- hms::as.hms(1L:100L)
+#' data <- met[  ,
+#'               list(t=t,
+#'                   x=rnorm(100),
+#'                   y=rnorm(100),
+#'                   eating=runif(100) > .5 ),
+#'               by="id"]
+#'
+#' d <- behavr(data,met)
+#' print(d)
+#' summary(d)
 NULL
 
 # Construction ------------------------------------------------------------
 
 #' @rdname behavr
-#' @details todo
-#' @param data todo
-#' @param metadata todo
+#' @param x [data.table] containing all measurments
+#' @param metadata [data.table] containing the metadata
+#' @param deep_copy logical defining whether the data is to be deep copied (the default). Otherwise, `x` and `metadata` are liked by reference.
+#' @details Both `x` and `metadata` should have a **column as a key** with **the same name** (typically named `id`).
 #' @export
-#library(data.table)
-#setClass("behavr", contains = "data.table")
-behavr <- function(x, metadata){
+behavr <- function(x, metadata, deep_copy=T){
   #todo, check input type/key/column names...!!
   # todo, run checks on meta data and data
   data.table::setattr(x,"metadata",metadata)
-  data.table::setattr(x,"class",c("behavr","data.table","data.frame")) 
+  data.table::setattr(x,"class",c("behavr","data.table","data.frame"))
 }
+
 
 meta <- function(d, with_n=T, with_t_range=T){
   attr(d,"metadata")
 }
 
-xmd <- function(var){
-  mc <- match.call(envir = parent.frame(n=2))
-  # todo check we call from within a [] env
-  d <- get("x",envir=parent.frame(n=3))
-  var <- deparse(substitute(var))
-  md <- meta(d)
-  col <- md[,c("id",var), with=F]
-  d[,.(id)][col][,var, with=F][[1]]
-}
 
 #-------------- Indexing
 
-"[.behavr" <- function(x,...){
+# @inheritDotParams data.table:::`[.data.table`
+# @rdname behavr
+"[.behavr" <- function(x, ...){
  out <- NextMethod()
- behavr(out,metadata=meta(x))
+ behavr(out, metadata = meta(x), deep_copy = F)
 }
---
+++ R/xmd.R
@@ -0,0 +1,45 @@
+#' Extract a variable from metadata and map it agains the data
+#'
+#' This function eXtract the MetaData from a parent [behavr] object.
+#' That is it expands a variable from the metadata to match data *by id*.
+#'
+#' @param var the variable to be extracted
+#' @return a vecor of the dame type as `var`, but of the same length as the number of row in the parent data.
+#' As each row of data is matched against metadata for this specific variable.
+#' @details This function *can only be called within between the `[]` of a parent* [behavr] object.
+#' It is intended to facilitate operations between data and metadata.
+#' For instance, if one want to modify a column of the data according the metadata.
+#' @examples
+#' library(data.table)
+#' set.seed(1)
+#' data <- data.table(
+#'                    id = rep(c("A","B"), times=c(10,26)),
+#'                    t = hms::hms(c(1:10,5:30)),
+#'                    x = rnorm(36), key="id"
+#'                    )
+#'
+#' metadata = data.table(id=c("A","B"), treatment=c("w","z"), lifespan=c(19,32), ref_x=c(1,0))
+#' dt <- behavr(data,metadata)
+#' summary(dt)
+#'
+#' #### subseting using metadata
+#' dt[xmd(treatment) == "w"]
+#' dt[xmd(lifespan) < 30]
+#'
+#' #### Allocating new columns using metadata
+#' # Just joining lifespan (not necessary)
+#' dt[, lif := xmd(lifespan)]
+#' print(dt)
+#' # Anonymously (more useful)
+#' dt[, x2 := x-xmd(ref_x)]
+#' print(dt)
+#' @export
+xmd <- function(var){
+  mc <- match.call(envir = parent.frame(n=2))
+  # todo check we call from within a [] env
+  d <- get("x",envir=parent.frame(n=3))
+  var <- deparse(substitute(var))
+  md <- meta(d)
+  col <- md[,c("id",var), with=F]
+  d[,.(id)][col][, var, with=F][[1]]
+}
