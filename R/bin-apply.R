2054d5b21d7652491b96e233c2914ff901ca8192
Quentin Geissmann <qgeissmann@gmail.com>
2017-08-07 15:54:57 +0100

    bin_apply allows time wrapping now

--- R/bin-apply.R
+++ R/bin-apply.R
@@ -6,13 +6,17 @@
 #'
 #' @param data [data.table] or [behavr] table (see details)
 #' @param y variable to be aggregated
-#' @param x the variable to be binned
-#' @param x_bin_length the length of the bins (same using as `x``)
+#' @param x variable to be binned
+#' @param x_bin_length length of the bins (same using as `x``)
+#' @param wrap_x_by numeric value defining wrapping period. `NULL` means no wrapping.
 #' @param FUN  function used to aggregate (e.g. [mean], [median], [sum] and so on)
 #' @param ... additional arguments to be passed to `FUN`
 #' @details
 #' `bin_apply` expects data from a single individal.
 #' `bin_apply_all` works on multiple individuals identifies by a unique key.
+#' `wrapping` is typically used to compute averages accros several periods.
+#' For instance,`wrap_x_by = days(1)`, means bins will aggreate values accross sevral days.
+#' The resulting x can be interpreted as "time relative to the onset of the day" (i.e. ZT).
 #' @examples
 #' query <- data.frame(experiment_id="toy_experiment",
 #'                       region_id=1:5)
@@ -26,21 +30,28 @@
 #' # More advanced usage
 #' dt <- toy_dam_data(query, duration=days(4))
 #'
-#' # nsum activity ber 60minutes
+#' # nsum activity per 60minutes
 #' dt_binned <- bin_apply_all(dt,
 #'                            activity,
 #'                            x=t,
 #'                            x_bin_length = mins(60),
 #'                            FUN=sum)
 #'
+#'
+#'#' # average activity. time in ZT
+#' dt_binned <- bin_apply_all(dt,
+#'                            activity,
+#'                            x=t,
+#'                            wrap_x_by=days(1)
+#'                            )
 #' @export
-bin_apply <- function(data, y, x=t, x_bin_length = mins(30), FUN=mean, ...){
+bin_apply <- function(data, y, x=t, x_bin_length = mins(30), wrap_x_by=NULL, FUN=mean, ...){
   var_name <- deparse(substitute(y))
   b_name <- deparse(substitute(x))
   data.table::setnames(data, c(var_name, b_name), c("..var..", "..b.."))
   tryCatch({
     out <- data[,.(..var.. = ..var..,
-                ..b.. = bin_var( ..b.., x_bin_length))]
+                ..b.. = bin_var( ..b.., x_bin_length, wrap=wrap_x_by))]
     out <- out[,
                     .(..var..=FUN(..var..,...)),
                     by=..b..]
@@ -60,7 +71,9 @@ bin_apply_all <- function(data, ...){
   data[, bin_apply(data=.SD, ...),by=eval(data.table::key(data))]
 }
 
-bin_var <- function(t, bin_length){
+bin_var <- function(t, bin_length, wrap=NULL){
+  if(!is.null(wrap))
+    t <- t %% wrap
   floor(t /bin_length) * bin_length
 }
 
