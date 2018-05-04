e011387e86c5dd7054202c410ef36e5283b77a1c
Quentin Geissmann <qgeissmann@gmail.com>
2018-05-04 10:34:43 +0100

    behavr.pdf dummy upate 2

--- .publish_doc.sh
+++ .publish_doc.sh
@@ -14,16 +14,14 @@ git config user.name "rapporter-travis"
 git config user.email "travis"
 
 ls 
-
-TRAVIS_EVENT_TYPE 
-
-
+echo $TRAVIS_EVENT_TYPE
 if [ $TRAVIS_EVENT_TYPE == "cron" ]
 then
   commit_message="CRON deployment on $TRAVIS_COMMIT [ci skip]"
 else
   commit_message="Deployment triggered by $TRAVIS_COMMIT [ci skip]"
 fi
+echo $commit_message
 
 cd 
 git clone -b master https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git repo.git
